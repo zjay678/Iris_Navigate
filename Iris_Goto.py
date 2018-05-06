d173472587768d95c2eac94d13a6e9b0e545e38d
Zoneshi <shizhongjiao@gmail.com>
2018-05-05 21:36:46 -0400

    bug fixed

--- Iris_Goto.py
+++ Iris_Goto.py
@@ -49,19 +49,20 @@ def arm_and_takeoff(vehicle, targetAlt = 1):
 
     print("Pre-arm checks")
     # Don't try to arm until autopilot is ready
-    while not vehicle.is_armable:
-        print(" Waiting for vehicle to initialise...")
-        time.sleep(1)
+    #while not vehicle.is_armable:
+    #    print(" Waiting for vehicle to initialise...")
+    #    time.sleep(1)
 
     print("Arming motors")
     # Copter should arm in GUIDED mode
-    vehicle.mode = dronekit.VehicleMode("GUIDED")
     vehicle.armed   = True
 
     # Confirm vehicle armed before attempting to take off
     while not vehicle.armed:
         print(" Waiting for arming...")
         time.sleep(1)
+
+    #vehicle.mode = dronekit.VehicleMode("GUIDED NO GPS")
 
     print("Taking off!")
     vehicle.simple_takeoff(targetAlt) # Take off to target altitude
